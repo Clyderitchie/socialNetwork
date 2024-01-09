b2ec5b330e760c65cdb21389560af6a04ba23207
francis <clyderitchie@yahoo.com>
2024-01-09 13:40:37 -0800

    Added the homepage and started on the wireframe of it and the ppost cards

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,18 @@
 const router = require('express').Router();
+const { Admin, Users } = require('../models')
 
-
+// Renders homepage 
+router.get('/', async (req, res) => {
+    try {
+        const userData = await Users.findAll();
+        const user = userData.map(p => p.get({ plain: true }));
+        res.render('homepage', {
+            ...user
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err.message);
+    }
+});
 
 module.exports = router; 
